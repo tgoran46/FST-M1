5aa337fb5d2c60a5c98fbbfffb98c118a69c98c4
Thriveni Gorantla46 <tgoran46@in.ibm.com>
2022-05-12 18:58:17 +0530

    Message

---
+++ Selenium/Activities/activity1.java
@@ -0,0 +1,16 @@
+package activities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class activity1 {
+     public static void main(String[] args) throws InterruptedException {
+         WebDriver driver = new FirefoxDriver();
+         driver.get("https://www.training-support.net");
+
+         Thread.sleep(1000);
+         driver.close();
+
+     }
+}
