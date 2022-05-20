474b92d372b0a957690b9a9033eb47f1942d1b8d
tgoran46 <104206697+tgoran46@users.noreply.github.com>
2022-05-20 21:29:17 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: activity1
+REM   activities
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
